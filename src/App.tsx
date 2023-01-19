20e6146db4a630ea7623952a7fb7c0250ccb1ed5
Ycow <skypnal12@gmail.com>
2023-01-19 18:01:07 +0900

    Feat : route react-query, 글로벌 스타일 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,31 @@
+import {
+  QueryCache,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import LoginPage from './pages/LoginPage';
+import LoginPage from './pages/LoginPage/LoginPage';
 
 const App = () => {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: 1,
+        refetchOnWindowFocus: false,
+        keepPreviousData: true,
+        staleTime: Infinity,
+      },
+    },
+  });
+
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <Routes>
-        <Route path='/login' element={<LoginPage />} />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <Routes>
+          <Route path='/login' element={<LoginPage />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
+import { theme } from './styles/theme';
 import App from './App';
+import GlobalStyled from './styles/Globalstyled';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyled />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 );
