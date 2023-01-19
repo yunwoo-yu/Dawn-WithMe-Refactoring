9f5c184a32fcab09eb18c231b59f5a30bd619952
Ycow <skypnal12@gmail.com>
2023-01-20 01:12:03 +0900

    Feat : api 데이터 형식에 맞게 body값 변경

--- src/api/user.ts
+++ src/api/user.ts
@@ -2,7 +2,7 @@ import { LoginData } from '../types/auth';
 import { instance } from './axiosBase';
 
 const userLogin = async (loginData: LoginData) => {
-  const response = await instance.post('/user/login', loginData);
+  const response = await instance.post('/user/login', { user: loginData });
 
   return response.data;
 };
