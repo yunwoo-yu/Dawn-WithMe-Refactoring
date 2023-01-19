7e24291ef90f8d39fb9dae5b6ef9c31c78e21c20
Ycow <skypnal12@gmail.com>
2023-01-19 17:58:52 +0900

    Feat : 로그인 폼 컴포넌트 추가

---
+++ src/components/organisms/LoginForm/LoginForm.tsx
@@ -0,0 +1,50 @@
+import { LoginProps } from '../../../types/auth';
+import Button from '../../atoms/Button/Button';
+import TextFiledCard from '../../molecules/TextFiledCard/TextFiledCard';
+import LoginFormWrapper from './styled';
+
+const LoginForm = ({
+  loginValue,
+  onChangeInputHandler,
+  onSubmitButtonHandler,
+  error,
+}: LoginProps) => {
+  const { email, password } = loginValue;
+
+  return (
+    <LoginFormWrapper onSubmit={onSubmitButtonHandler}>
+      <TextFiledCard
+        value={email}
+        type='text'
+        id='email'
+        name='email'
+        onChange={onChangeInputHandler}
+        autoFocus
+        placeholder='이메일을 입력해주세요.'
+      >
+        이메일
+      </TextFiledCard>
+      <TextFiledCard
+        value={password}
+        type='password'
+        id='password'
+        name='password'
+        placeholder='패스워드를 입력해주세요. (6자이상)'
+        onChange={onChangeInputHandler}
+      >
+        패스워드
+      </TextFiledCard>
+      {error && <p>{error}</p>}
+      <Button
+        width='100%'
+        size='large'
+        type='submit'
+        disabled={!email || !password}
+      >
+        로그인
+      </Button>
+    </LoginFormWrapper>
+  );
+};
+
+export default LoginForm;
