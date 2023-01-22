423dc4b9ad24ffd49c826eeeb6fc19498e8bb1d6
Ycow <skypnal12@gmail.com>
2023-01-23 03:43:12 +0900

    Feat : useForm error 핸들링 가능하도록 추가

--- src/hooks/useForm.ts
+++ src/hooks/useForm.ts
@@ -1,15 +1,18 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
+import isValidate from '../util/isValidate';
 
 const useForm = <T extends object>(initialState: T) => {
-  const [formData, setFormData] = useState(initialState);
+  const [formData, setFormData] = useState<T>({ ...initialState });
+  const [error, setError] = useState<T>({ ...initialState });
 
   const onChangeInputHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
 
     setFormData({ ...formData, [name]: value });
+    isValidate(name, value, setError);
   };
 
-  return { formData, onChangeInputHandler };
+  return { formData, error, onChangeInputHandler };
 };
 
 export default useForm;
