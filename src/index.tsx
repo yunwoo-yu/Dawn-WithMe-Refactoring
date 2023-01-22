4b309d593731d19f9c0f3c7c4f4c0d4f7746d8d3
Ycow <skypnal12@gmail.com>
2023-01-23 03:40:11 +0900

    Feat : fontface 추가 및 toast 모달 창 도입 font 크기 변경

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import { ThemeProvider } from 'styled-components';
 import { theme } from './styles/theme';
 import App from './App';
 import GlobalStyled from './styles/Globalstyled';
+import './index.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
