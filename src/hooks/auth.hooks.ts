a0427b918bc96a4dd203c16c60290db36f2b047e
Ycow <skypnal12@gmail.com>
2023-01-23 03:42:16 +0900

    Feat : useLoginMutation 수정 및 useEmailValidMutation 커스텀훅 추가

--- src/hooks/auth.hooks.ts
+++ src/hooks/auth.hooks.ts
@@ -1,7 +1,9 @@
 import { useMutation } from '@tanstack/react-query';
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import userLogin from '../api/user';
+
+import { userEmailValid, userLogin } from '../api/auth';
+import { errorToast } from '../util/toast';
 
 export const useLoginMutation = (
   setError: React.Dispatch<React.SetStateAction<string>>,
@@ -12,22 +14,43 @@ export const useLoginMutation = (
     onSuccess(resData) {
       if (resData.status === 422) {
         setError(`${resData.message}`);
-        return;
-      }
-      if (resData.message === '잘못된 접근입니다.') {
+      } else if (resData.message === '잘못된 접근입니다.') {
         setError(`${resData.message}`);
-        return;
+      } else if (resData.status === 404) {
+        errorToast(`서버에 문제가 있습니다. 잠시 후 시도해주세요 :(`);
+      } else {
+        localStorage.setItem('accountname', resData.user.accountname);
+        localStorage.setItem('token', resData.user.token);
+        navigate('/home');
       }
-      localStorage.setItem('accountname', resData.user.accountname);
-      localStorage.setItem('token', resData.user.token);
-      navigate('/home');
     },
-    onError(err: any) {
-      setError(`${err.message}`);
+    onError(err) {
+      console.log(err);
     },
   });
 };
 
-export const userSignupMutation = () => {
-  // return useMutation();
+export const useEmailValidMutation = (data: object) => {
+  const navigate = useNavigate();
+
+  return useMutation(userEmailValid, {
+    onSuccess(resData) {
+      if (resData.message === '잘못된 접근입니다.') {
+        errorToast(`${resData.message}`);
+      } else if (resData.status === 404) {
+        errorToast(`서버에 문제가 있습니다. 잠시 후 시도해주세요 :(`);
+      } else if (resData.message === '이미 가입된 이메일 주소 입니다.') {
+        errorToast(`${resData.message}`);
+      } else if (resData.message === '사용 가능한 이메일 입니다.') {
+        navigate('/profilesetting', {
+          state: {
+            data: { ...data },
+          },
+        });
+      }
+    },
+    onError(err) {
+      console.log(err);
+    },
+  });
 };
