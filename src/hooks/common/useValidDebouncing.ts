e4c5e7cac750f304e298a92498f8d684516de240
Ycow <skypnal12@gmail.com>
2023-01-26 20:38:38 +0900

    Feat : debounce 커스텀 훅 추가

---
+++ src/hooks/common/useValidDebouncing.ts
@@ -0,0 +1,23 @@
+import { UseMutationResult } from '@tanstack/react-query';
+import { debounce } from 'lodash';
+import { useCallback, useEffect } from 'react';
+
+const useValidDebouncing = (
+  mutationFn: UseMutationResult<any, any, string, unknown>,
+  formData: string,
+) => {
+  const debounceEmailValidMutation = useCallback(
+    debounce((data) => {
+      mutationFn.mutate(data);
+    }, 500),
+    [],
+  );
+
+  useEffect(() => {
+    if (formData) {
+      debounceEmailValidMutation(formData);
+    }
+  }, [formData]);
+};
+
+export default useValidDebouncing;
