14bd7e7e5aa960492f5fce1b9985cdc3497bcec8
Ycow <skypnal12@gmail.com>
2023-01-26 20:42:08 +0900

    Feat : axios 함수 추가

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,5 +1,5 @@
 import { LoginSignupData } from '../types/auth';
-import { instance } from './axiosBase';
+import { imgInstance, instance } from './axiosBase';
 
 export const userLogin = async (loginData: LoginSignupData) => {
   const response = await instance.post('/user/login', { user: loginData });
@@ -7,9 +7,32 @@ export const userLogin = async (loginData: LoginSignupData) => {
   return response.data;
 };
 
-export const userEmailValid = async (signupData: string) => {
+export const signupEmailValid = async (signupData: string) => {
   const response = await instance.post('/user/emailvalid', {
     user: { email: signupData },
+  });
+
+  return response.data;
+};
+
+export const uploadUserImage = async (image: FormData) => {
+  const response = await imgInstance.post('/image/uploadfile', image);
+
+  return response.data;
+};
+
+export const signupAccountNameValid = async (signupData: string) => {
+  const response = await instance.post('/user/accountnamevalid', {
+    user: { accountname: signupData },
+  });
+
+  return response.data;
+};
+
+export const uesrSignup = async (signData: LoginSignupData) => {
+  console.log(signData);
+  const response = await instance.post('/user', {
+    user: { ...signData },
   });
 
   return response.data;
