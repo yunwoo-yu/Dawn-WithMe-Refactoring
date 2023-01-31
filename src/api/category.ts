981054de71d11d958ae2d6ee97c93f6d59ce2125
Ycow <skypnal12@gmail.com>
2023-01-31 23:48:38 +0900

    Feat : axios following 상품 불러오는 로직 추가

---
+++ src/api/category.ts
@@ -0,0 +1,25 @@
+import { FeedData } from '../types/category';
+import { accessInstance } from './axiosBase';
+
+export const getFollowingProduct = async (accountNameList: string[]) => {
+  const accountName = localStorage.getItem('accountname');
+  const result: FeedData[] = [];
+
+  if (accountName) {
+    accountNameList.push(accountName);
+  }
+
+  const promises = accountNameList.map(async (username) => {
+    const response = await accessInstance.get(`/product/${username}`);
+
+    if (response.data.data) {
+      response.data.product.forEach((el: FeedData) => result.push(el));
+    }
+  });
+
+  await Promise.all(promises);
+
+  return result;
+};
+
+export default getFollowingProduct;
