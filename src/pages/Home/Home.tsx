b0d13a6cfb20dccabc92bbed1be38e38b958e082
Ycow <skypnal12@gmail.com>
2023-02-02 23:05:19 +0900

    Feat : HeaderType 선언 유틸 추가

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,19 +1,30 @@
 import DefaultLayout, {
   HeaderStyle,
 } from '../../components/common/Layout/DefaultLayout/DefaultLayout';
-import CategoryList from '../../components/Home/CategoryList/CategoryList';
+import CategoryItem from '../../components/Home/CategoryItem/CategoryItem';
+
+import useGetCategoryFeedQuery from '../../hooks/category.hooks';
+import { FeedData } from '../../types/category';
 import HomeWrapper from './styled';
 
 const Home = () => {
-  const styleProps: HeaderStyle = {
+  const styleProps: Partial<HeaderStyle> = {
     title: '카테고리 피드',
     isTitle: true,
     isMoreButton: true,
   };
+  const { data: feedData, isLoading } = useGetCategoryFeedQuery();
+
+  if (isLoading) return <p>로딩 중</p>;
+
   return (
     <DefaultLayout styleProps={styleProps}>
       <HomeWrapper>
-        <CategoryList />
+        <ul>
+          {feedData?.map((item: FeedData) => (
+            <CategoryItem key={item.id} data={item} />
+          ))}
+        </ul>
       </HomeWrapper>
     </DefaultLayout>
   );
