0d3b63f43bf73ee9ea044af9783ce10e650a2ed8
Ycow <skypnal12@gmail.com>
2023-02-11 00:42:15 +0900

    feat: import 변경

--- src/hooks/common/useForm.ts
+++ src/hooks/common/useForm.ts
@@ -1,6 +1,6 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import { useRecoilState } from 'recoil';
-import errorMessageAtom from '../../recoil/atom';
+import { errorMessageAtom } from '../../recoil/atom';
 import isValidate from '../../util/isValidate';
 
 const useForm = <T extends object>(initialState: T) => {
