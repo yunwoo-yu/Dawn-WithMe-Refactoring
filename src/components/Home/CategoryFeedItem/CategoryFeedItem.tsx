3a57a52d3a176b6455dfa0124690876cb8cf201a
Ycow <skypnal12@gmail.com>
2023-02-12 04:38:25 +0900

    feat: 카테고리 스터디 일 경우와 내 게시글일 경우 구분해서 더보기버튼, 참여하기 버튼 추가

--- src/components/Home/CategoryFeedItem/CategoryFeedItem.tsx
+++ src/components/Home/CategoryFeedItem/CategoryFeedItem.tsx
@@ -1,13 +1,15 @@
 import { Link } from 'react-router-dom';
-
+import { useRecoilValue } from 'recoil';
+import MoreIcon from '../../../assets/images/icon-more-vertical.png';
 import CategoryItemWrapper from './styled';
 import profileImg from '../../../assets/images/profile-logo.png';
 import { FeedData } from '../../../types/category';
+import { selectBoxValueAtom } from '../../../recoil/atom';
 
 const CategoryFeedItem = ({ data }: { data: FeedData }) => {
-  const { itemImage, createdAt, price, link, id, author } = data;
+  const { itemImage, createdAt, price, link, id, author, itemName } = data;
   const { accountname, username, image: profileImage } = author;
-
+  const selectValue = useRecoilValue(selectBoxValueAtom);
   const basicProfileImg =
     profileImage === 'http://146.56.183.55:5050/Ellipse.png'
       ? profileImg
@@ -17,20 +19,38 @@ const CategoryFeedItem = ({ data }: { data: FeedData }) => {
 
   return (
     <CategoryItemWrapper>
-      <Link className='profile' to={`/profile/${accountname}`}>
-        <img
-          className='profile-image'
-          src={basicProfileImg}
-          alt='프로필 이미지'
-        />
-        <div className='user-box'>
-          <p className='user-name'>{username}</p>
-          <p className='user-accountname'>@ {accountname}</p>
-        </div>
-      </Link>
+      <div className='profile-box'>
+        <Link className='profile' to={`/profile/${accountname}`}>
+          <img
+            className='profile-image'
+            src={basicProfileImg}
+            alt='프로필 이미지'
+          />
+          <div className='user-box'>
+            <p className='user-name'>{username}</p>
+            <p className='user-accountname'>@ {accountname}</p>
+          </div>
+        </Link>
+        {localStorage.getItem('accountname') !== accountname &&
+          itemName === 'study' && (
+            <Link className='participation' to='/chatdetail'>
+              참여하기
+            </Link>
+          )}
+        {localStorage.getItem('accountname') === accountname && (
+          <button type='button' className='more-btn'>
+            <img src={MoreIcon} alt='게시글 더보기 버튼' />
+          </button>
+        )}
+      </div>
       <Link className='feed-content-box' to={`/home/detail/${id}`}>
         <p className='feed-text'>{link}</p>
         <img className='feed-img' src={itemImage} alt='게시물 이미지' />
+        {selectValue === 'study' && (
+          <p className='personnel'>
+            참여 인원 {Math.ceil(price / 2)}/<span>{price}</span>
+          </p>
+        )}
         <p className='feed-date'>
           {postDate[0]}년 {postDate[1]}월 {parseInt(postDate[2], 10)}일
         </p>
--- src/components/Home/CategoryFeedItem/styled.ts
+++ src/components/Home/CategoryFeedItem/styled.ts
@@ -3,38 +3,68 @@ import styled, { css } from 'styled-components';
 const CategoryItemWrapper = styled.li`
   width: 100%;
   margin: 20px 0;
+  text-align: left;
 
   ${({ theme }) => {
     return css`
-      .profile {
+      .profile-box {
         display: flex;
+        justify-content: space-between;
         align-items: center;
-        .profile-image {
-          width: 42px;
-          height: 42px;
-          border-radius: 50%;
-          object-fit: cover;
-        }
-        .user-box {
-          display: flex;
-          flex-direction: column;
-          align-items: flex-start;
-          gap: 4px;
-          margin-left: 12px;
 
-          .user-name {
-            color: ${theme.colors.colorBorder};
-            font-weight: 500;
-            font-size: ${theme.fontSizes.base};
+        .participation {
+          display: inline-block;
+          height: 36px;
+          font-size: ${theme.fontSizes.base};
+          padding: 10px 12px;
+          border: 1px solid ${theme.colors.colorWhite};
+          border-radius: 18px;
+          line-height: 14px;
+          &:hover {
+            background-color: ${theme.colors.colorMain};
+            border: 1px solid ${theme.colors.colorMain};
           }
+        }
 
-          .user-accountname {
-            color: ${theme.colors.color76};
-            font-size: ${theme.fontSizes.smallMid};
+        .more-btn {
+          width: 18px;
+          height: 18px;
+
+          > img {
+            width: 100%;
+          }
+        }
+
+        .profile {
+          display: inline-flex;
+          align-items: center;
+
+          .profile-image {
+            width: 42px;
+            height: 42px;
+            border-radius: 50%;
+            object-fit: cover;
+          }
+          .user-box {
+            display: flex;
+            flex-direction: column;
+            align-items: flex-start;
+            gap: 4px;
+            margin-left: 12px;
+
+            .user-name {
+              color: ${theme.colors.colorBorder};
+              font-weight: 500;
+              font-size: ${theme.fontSizes.base};
+            }
+
+            .user-accountname {
+              color: ${theme.colors.color76};
+              font-size: ${theme.fontSizes.smallMid};
+            }
           }
         }
       }
-
       .feed-content-box {
         display: block;
         margin-left: 54px;
@@ -44,13 +74,13 @@ const CategoryItemWrapper = styled.li`
           color: ${theme.colors.colorWhite};
           font-size: ${theme.fontSizes.xl};
           line-height: 18px;
-          margin-top: 16px;
+          margin-top: 12px;
         }
 
         .feed-img {
           width: 100%;
           max-height: 300px;
-          margin-top: 16px;
+          margin-top: 14px;
           border-radius: 16px;
           object-fit: cover;
           border: 0.5px solid ${theme.colors.colorNavBg};
@@ -60,6 +90,18 @@ const CategoryItemWrapper = styled.li`
           color: ${theme.colors.colorBorder};
           font-size: ${theme.fontSizes.small};
           margin-top: 16px;
+        }
+
+        .personnel {
+          font-size: ${theme.fontSizes.base};
+          margin: 16px 0;
+
+          & {
+            text-align: right;
+          }
+          span {
+            color: ${theme.colors.colorMain};
+          }
         }
       }
     `;
