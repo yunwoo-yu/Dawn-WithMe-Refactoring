f36943afa441b8785cd5d2adfda4b924d5835e8b
Ycow <skypnal12@gmail.com>
2023-02-17 00:31:54 +0900

    feat: 카테고리 피드 스켈레톤ui 수정

--- src/components/Home/CategoryFeedItem/CategoryFeedItemSkeleton.tsx
+++ src/components/Home/CategoryFeedItem/CategoryFeedItemSkeleton.tsx
@@ -23,29 +23,37 @@ const CategoryFeedItemSkeleton = () => {
         </div>
       </CategoryItemWrapper>
       <CategoryItemWrapper>
-        <div className='profile'>
-          <Skeleton width={42} height={42} circle />
-          <div className='user-box'>
-            <Skeleton width={100} height={10} count={2} />
+        <div className='profile-box'>
+          <div className='profile'>
+            <Skeleton width={42} height={42} circle />
+            <div className='user-box'>
+              <Skeleton width={100} height={10} count={2} />
+            </div>
           </div>
         </div>
         <div className='feed-content-box'>
           <Skeleton className='feed-text' height={18} borderRadius={18} />
-          <Skeleton className='feed-img' height={300} />
-          <Skeleton className='feed-date' width={80} height={10} />
+          <div className='img-box'>
+            <Skeleton className='feed-img' height={300} />
+          </div>
+          <Skeleton className='feed-date' height={10} count={2} />
         </div>
       </CategoryItemWrapper>
       <CategoryItemWrapper>
-        <div className='profile'>
-          <Skeleton width={42} height={42} circle />
-          <div className='user-box'>
-            <Skeleton width={100} height={10} count={2} />
+        <div className='profile-box'>
+          <div className='profile'>
+            <Skeleton width={42} height={42} circle />
+            <div className='user-box'>
+              <Skeleton width={100} height={10} count={2} />
+            </div>
           </div>
         </div>
         <div className='feed-content-box'>
           <Skeleton className='feed-text' height={18} borderRadius={18} />
-          <Skeleton className='feed-img' height={300} />
-          <Skeleton className='feed-date' width={80} height={10} />
+          <div className='img-box'>
+            <Skeleton className='feed-img' height={300} />
+          </div>
+          <Skeleton className='feed-date' height={10} count={2} />
         </div>
       </CategoryItemWrapper>
     </ul>
