5983c9a8ce667ac01d6cb5c3e77e7ba62124c5f9
Ycow <skypnal12@gmail.com>
2023-02-17 00:32:11 +0900

    feat: 자유게시판 타입 post 추가

--- src/types/freeBoard.ts
+++ src/types/freeBoard.ts
@@ -14,5 +14,6 @@ export interface FreeBoardDataTypes {
 }
 
 export interface FreeBoardListDataTypes {
-  post: FreeBoardDataTypes[];
+  post?: FreeBoardDataTypes[];
+  posts?: FreeBoardDataTypes[];
 }
