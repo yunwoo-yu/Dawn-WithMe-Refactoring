ba924787eab6e89e14d9dbdab9dc6a2721267364
Ycow <skypnal12@gmail.com>
2023-02-20 23:19:54 +0900

    feat: freeboard List skeleton fix

--- src/components/FreeBoard/FreeBoardItem/FreeBoardItemSkeleton.tsx
+++ src/components/FreeBoard/FreeBoardItem/FreeBoardItemSkeleton.tsx
@@ -1,5 +1,4 @@
 import Skeleton from 'react-loading-skeleton';
-import ProfileFreeBoardPostListWrapper from '../../Profile/ProfileFreeBoardPostList/styled';
 import FreeBoardItemWrapper from './styled';
 
 const FreeBoardItemSkeleton = () => {
@@ -17,7 +16,27 @@ const FreeBoardItemSkeleton = () => {
         <div className='feed-content-box'>
           <Skeleton className='feed-text' height={18} borderRadius={18} />
           <div className='img-box'>
-            <Skeleton className='feed-img' height={300} />
+            <Skeleton className='feed-img' />
+          </div>
+          <div className='comment-heart-box'>
+            <Skeleton className='feed-date' width={80} height={20} />
+          </div>
+          <Skeleton className='feed-date' width={80} height={10} />
+        </div>
+      </FreeBoardItemWrapper>
+      <FreeBoardItemWrapper>
+        <div className='profile-box'>
+          <div className='profile'>
+            <Skeleton width={42} height={42} circle />
+            <div className='user-box'>
+              <Skeleton width={100} height={10} count={2} />
+            </div>
+          </div>
+        </div>
+        <div className='feed-content-box'>
+          <Skeleton className='feed-text' height={18} borderRadius={18} />
+          <div className='img-box'>
+            <Skeleton className='feed-img' />
           </div>
           <div className='comment-heart-box'>
             <Skeleton className='feed-date' width={80} height={20} />
