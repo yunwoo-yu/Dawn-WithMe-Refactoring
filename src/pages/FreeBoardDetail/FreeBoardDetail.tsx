0660dc3545089beb950650e9bed2c6bd9476e00f
Ycow <skypnal12@gmail.com>
2023-02-23 01:33:18 +0900

    feat: freeBaord detail components add

---
+++ src/pages/FreeBoardDetail/FreeBoardDetail.tsx
@@ -0,0 +1,24 @@
+import DefaultLayout, {
+  HeaderStyle,
+} from '../../components/common/Layout/DefaultLayout/DefaultLayout';
+import FreeBoardDetailFeed from '../../components/FreeBoard/FreeBoardDetailFeed/FreeBoardDetailFeed';
+import FreeBoardDetailWrapper from './styled';
+
+const freeBoardDetailHeaderProps: Partial<HeaderStyle> = {
+  title: '자유게시판 게시물',
+  isTitle: true,
+  isBackButton: true,
+  isMoreButton: true,
+};
+
+const FreeBoardDetail = () => {
+  return (
+    <DefaultLayout styleProps={freeBoardDetailHeaderProps}>
+      <FreeBoardDetailWrapper>
+        <FreeBoardDetailFeed />
+      </FreeBoardDetailWrapper>
+    </DefaultLayout>
+  );
+};
+
+export default FreeBoardDetail;
---
+++ src/pages/FreeBoardDetail/styled.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+import AuthWrapper from '../../components/common/Wrapper/AuthWrapper';
+
+const FreeBoardDetailWrapper = styled(AuthWrapper)`
+  overflow: scroll;
+  padding: 20px 16px 120px;
+`;
+
+export default FreeBoardDetailWrapper;
