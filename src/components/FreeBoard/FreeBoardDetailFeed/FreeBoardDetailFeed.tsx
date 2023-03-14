d6adb1060f1d7125dda260d89884a940141e02b4
Ycow <skypnal12@gmail.com>
2023-03-14 23:18:21 +0900

    feat: DetailFeed 에 쌓인 컴포넌트 분리

--- src/components/FreeBoard/FreeBoardDetailFeed/FreeBoardDetailFeed.tsx
+++ src/components/FreeBoard/FreeBoardDetailFeed/FreeBoardDetailFeed.tsx
@@ -1,43 +1,13 @@
-import { useState } from 'react';
-import {
-  useGetDetailFeedCommentQuery,
-  useGetFreeBoardDetailFeedQuery,
-} from '../../../hooks/freeBoard.hooks';
-import { useGetMyProfileDataQuery } from '../../../hooks/profile.hooks';
-import {
-  FreeBoardCommentDataTypes,
-  FreeBoardCommentListDataTypes,
-} from '../../../types/freeBoard';
-import FreeBoardCommentAddForm from '../FreeBoardCommentAddForm/FreeBoardCommentAddForm';
-import FreeBoardCommentItem from '../FreeBoardCommentItem/FreeBoardCommentItem';
+import { useGetFreeBoardDetailFeedQuery } from '../../../hooks/freeBoard.hooks';
 import FreeBoardItem from '../FreeBoardItem/FreeBoardItem';
 import FreeBoardDetailFeedWrapper from './styled';
 
 const FreeBoardDetailFeed = () => {
   const { data } = useGetFreeBoardDetailFeedQuery();
-  const [sortedCommentData, setSortedCommentData] =
-    useState<FreeBoardCommentDataTypes[]>();
-  const { data: _commentData } = useGetDetailFeedCommentQuery({
-    suspense: false,
-    useErrorBoundary: false,
-    onSuccess(comments: FreeBoardCommentListDataTypes) {
-      setSortedCommentData(comments.comments.reverse());
-    },
-  });
-  const { data: myProfile } = useGetMyProfileDataQuery({
-    suspense: false,
-    useErrorBoundary: false,
-  });
 
   return (
     <FreeBoardDetailFeedWrapper>
       {data && <FreeBoardItem as='div' data={data.post} />}
-      <ul className='comment-list'>
-        {sortedCommentData?.map((item: FreeBoardCommentDataTypes) => (
-          <FreeBoardCommentItem data={item} key={item.id} />
-        ))}
-      </ul>
-      <FreeBoardCommentAddForm profileImg={myProfile?.image} />
     </FreeBoardDetailFeedWrapper>
   );
 };
