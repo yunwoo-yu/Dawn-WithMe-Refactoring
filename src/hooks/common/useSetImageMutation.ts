87046830a31c63a7d2dfbe8ae9b552fa841b5b82
Ycow <skypnal12@gmail.com>
2023-03-18 21:22:51 +0900

    feat: 이미지 업로드 에러 모달 추가

--- src/hooks/common/useSetImageMutation.ts
+++ src/hooks/common/useSetImageMutation.ts
@@ -1,5 +1,6 @@
 import { useMutation } from '@tanstack/react-query';
 import { useRef, useState } from 'react';
+import { toast } from 'react-toastify';
 import { url } from '../../api/axiosBase';
 import setFeedImagePost from '../../api/common';
 
@@ -19,8 +20,10 @@ const useSetImageMutation = () => {
 
   const imageUploadMutation = useMutation(setFeedImagePost, {
     onSuccess(data) {
-      console.log(data);
       setImgSrc(`${url}/1678808411473.png`);
+    },
+    onError(error) {
+      toast.error('이미지 업로드에 실패했습니다. 다시 시도해주세요!');
     },
   });
 
