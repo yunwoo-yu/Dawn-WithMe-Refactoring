56474e46498b9d1f4406b89af2948b15d9bde44e
Ycow <skypnal12@gmail.com>
2023-03-20 03:39:42 +0900

    feat: 자유게시판 글쓰기 폼 컴포넌트 추가

--- src/components/FreeBoard/FreeBoardCreatePostForm/FreeBoardCreatePostForm.tsx
+++ src/components/FreeBoard/FreeBoardCreatePostForm/FreeBoardCreatePostForm.tsx
@@ -1,8 +1,50 @@
+import ReactTextareaAutosize from 'react-textarea-autosize';
+import useSetImageMutation from '../../../hooks/common/useSetImageMutation';
+import { useGetMyProfileDataQuery } from '../../../hooks/profile.hooks';
 import FreeBoardCreatePostFormWrapper from './styled';
 
+import uploadIcon from '../../../assets/images/upload-file.png';
+import deleteIcon from '../../../assets/images/icon-delete.png';
+
 const FreeBoardCreatePostForm = () => {
+  const { data } = useGetMyProfileDataQuery();
+  const { onClickImageFileModalHandler, imageInputRef, onChangeInputImage, imgSrc, setImgSrc } =
+    useSetImageMutation();
+
+  const onClickImgDeleteHandler = (imgString: string) => {
+    setImgSrc(imgSrc.filter((el) => el !== imgString));
+  };
+
   return (
-    <FreeBoardCreatePostFormWrapper id='freeBoardPostAddForm'>zz</FreeBoardCreatePostFormWrapper>
+    <>
+      <FreeBoardCreatePostFormWrapper id='freeBoardPostAddForm'>
+        <div className='text-box'>
+          <img src={data?.image} alt='프로필 이미지' />
+          <ReactTextareaAutosize placeholder='게시글 입력하기...' />
+        </div>
+        <ul className='img-box'>
+          {imgSrc.map((el) => (
+            <li key={el} className={imgSrc.length > 1 ? 'multiple-img' : ''}>
+              <img src={el} alt='게시물 이미지' />
+              <button type='button' onClick={() => onClickImgDeleteHandler(el)}>
+                <img src={deleteIcon} alt='삭제 버튼' />
+              </button>
+            </li>
+          ))}
+        </ul>
+        <input
+          type='file'
+          multiple
+          accept='image/*'
+          id='imageUpload'
+          ref={imageInputRef}
+          onChange={onChangeInputImage}
+        />
+      </FreeBoardCreatePostFormWrapper>
+      <button type='button' className='upload-button' onClick={onClickImageFileModalHandler}>
+        <img src={uploadIcon} alt='이미지 업로드 버튼' />
+      </button>
+    </>
   );
 };
 
--- src/components/FreeBoard/FreeBoardCreatePostForm/styled.ts
+++ src/components/FreeBoard/FreeBoardCreatePostForm/styled.ts
@@ -1,5 +1,79 @@
 import styled from 'styled-components';
 
-const FreeBoardCreatePostFormWrapper = styled.form``;
+const FreeBoardCreatePostFormWrapper = styled.form`
+  /* display: flex;
+  flex-wrap: wrap; */
+  position: relative;
+  padding: 0 16px;
+
+  .text-box {
+    width: 100%;
+    height: 100%;
+    display: flex;
+
+    > img {
+      width: 42px;
+      height: 42px;
+      border-radius: 50%;
+    }
+
+    textarea {
+      width: 100%;
+      padding: 0;
+      margin-left: 16px;
+      background-color: ${({ theme }) => theme.colors.colorBg};
+      border: none;
+      resize: none;
+
+      &:focus {
+        outline: none;
+      }
+    }
+  }
+
+  #imageUpload {
+    display: none;
+  }
+
+  > ul {
+    width: 100%;
+    display: flex;
+    padding: 16px 0 0 54px;
+    gap: 12px;
+    overflow-x: scroll;
+
+    li {
+      width: 100%;
+      height: 240px;
+      position: relative;
+
+      &.multiple-img {
+        min-width: 168px;
+        width: 168px;
+        height: 126px;
+      }
+
+      > img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+        border-radius: 10px;
+      }
+
+      > button {
+        position: absolute;
+        width: 22px;
+        height: 25px;
+        top: 0;
+        right: 0;
+
+        > img {
+          width: 100%;
+          height: 100%;
+        }
+      }
+    }
+  }
+`;
 
 export default FreeBoardCreatePostFormWrapper;
