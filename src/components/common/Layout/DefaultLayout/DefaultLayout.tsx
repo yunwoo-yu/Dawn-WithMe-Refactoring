95e4711b3fe8a9bd03eead1f03970e1513564ac5
Ycow <skypnal12@gmail.com>
2023-04-04 08:50:53 +0900

    feat: 모달, 얼럿 추가

--- src/components/common/Layout/DefaultLayout/DefaultLayout.tsx
+++ src/components/common/Layout/DefaultLayout/DefaultLayout.tsx
@@ -1,6 +1,10 @@
-import React from 'react';
-
+import React, { Suspense } from 'react';
+import { useRecoilValue } from 'recoil';
+import { isShowAlertAtom, isShowModalAtom } from '../../../../recoil/atom';
+import Alert from '../../Alert/Alert';
 import TopNavBar from '../../Header/TopNavBar';
+import Modal from '../../Modal/Modal';
+import RetryErrorBoundary from '../../RetryErrorBoundary/RetryErrorBoundary';
 import TabMenu from '../../TabMenuBar/TabMenuBar';
 import DefaultMainWrapper from '../../Wrapper/DefaultWrapper';
 
@@ -27,11 +31,24 @@ const DefaultLayout = ({
   styleProps: Partial<HeaderStyle>;
 }) => {
   const { title, isTabMenu = true } = styleProps;
+  const isShowModal = useRecoilValue(isShowModalAtom);
+  const isShowAlert = useRecoilValue(isShowAlertAtom);
+
   return (
     <>
       <TopNavBar styleProps={styleProps}>{title}</TopNavBar>
       <DefaultMainWrapper>{children}</DefaultMainWrapper>
       {isTabMenu && <TabMenu />}
+      {(isShowModal.isActive.header ||
+        isShowModal.isActive.comment ||
+        isShowModal.isActive.post) && <Modal />}
+      <RetryErrorBoundary>
+        <Suspense>
+          {(isShowAlert.isActive.header ||
+            isShowAlert.isActive.comment ||
+            isShowAlert.isActive.post) && <Alert />}
+        </Suspense>
+      </RetryErrorBoundary>
     </>
   );
 };
