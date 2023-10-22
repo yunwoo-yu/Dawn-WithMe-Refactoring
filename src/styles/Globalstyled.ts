e5738450e1b69316c6040205e1173712d7f681f9
YunCow <skypnal12@gmail.com>
2023-10-22 22:31:25 +0900

    fix: hiiden class 수정

--- src/styles/Globalstyled.ts
+++ src/styles/Globalstyled.ts
@@ -45,7 +45,7 @@ button{
     width: 1px;
     height: 1px;
     border: 0;
-    clip: rect(1px, 1px, 1px, 1px);
+    clip: rect(0,0,0,0);
     clip-path: inset(50%);
     word-break: initial;
     word-wrap: initial;
